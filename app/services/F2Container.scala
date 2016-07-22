bca1ea568f66c229b27f9cb849f10867600a89e5
Yurii Dalekorei <yuriy.dalekorey@gmail.com>
2016-07-22 22:08:41 +0300

    Implemented functionality to read and write values from f2 csv file

--- app/services/F2Container.scala
+++ app/services/F2Container.scala
@@ -1,5 +1,10 @@
 package services
 
+import java.io.{File, PrintWriter}
+import javax.inject.Singleton
+
+import scala.io.Source
+
 /**
   * Created by ydalekorey on 7/22/16.
   */
@@ -7,4 +12,31 @@ trait F2Container {
   def writeByIndex(v4: Int, i: Int): Unit
 
   def getValueByIndex(v1: Int): Int
+}
+
+@Singleton
+class CsvFileF2Container(val f2Location: String) extends F2Container {
+
+  override def writeByIndex(v4: Int, i: Int): Unit = {
+    val bufferedSource = Source.fromFile(f2Location)
+    val numbers = bufferedSource.getLines.next().split(",").map(_.trim).toVector
+    bufferedSource.close
+
+    val updatedNumbers = numbers updated(v4, i.toString)
+
+    val pw = new PrintWriter(new File(f2Location))
+    val fileContent = updatedNumbers.mkString(", ")
+    pw.write(fileContent)
+    pw.close
+  }
+
+  override def getValueByIndex(v1: Int): Int = {
+    val bufferedSource = Source.fromFile(f2Location)
+
+    val numbers = bufferedSource.getLines.next().split(",").map(_.trim).map(_.toInt).toVector
+
+    bufferedSource.close
+
+    numbers(v1)
+  }
 }
---
+++ test/services/CsvFileF2ContainerSpec.scala
@@ -0,0 +1,50 @@
+package services
+
+import java.io.{File, PrintWriter}
+
+import org.scalatest.BeforeAndAfter
+import org.scalatest.mock.MockitoSugar
+import org.scalatestplus.play.PlaySpec
+
+/**
+  * Created by ydalekorey on 7/22/16.
+  */
+class CsvFileF2ContainerSpec extends PlaySpec with MockitoSugar with BeforeAndAfter {
+
+  private var f2Container: F2Container = _
+
+  before {
+    val f2File = new File("f2.csv")
+    val content = "1, 2, 3, 4, 5"
+    createTestFile(f2File, content)
+
+    f2Container = new CsvFileF2Container(f2File.getAbsolutePath)
+
+  }
+
+
+  "CsvFileF2Container" should {
+    "read appropriate value from specified column in csv file" in {
+
+      f2Container.getValueByIndex(3) must equal(4)
+
+    }
+  }
+
+  "CsvFileF2Container" should {
+    "write value to specified column in csv file" in {
+
+      f2Container.writeByIndex(3, 10)
+
+      f2Container.getValueByIndex(3) must equal(10)
+
+    }
+  }
+
+  def createTestFile(f2File: File, content: String): Unit = {
+    val pw = new PrintWriter(f2File)
+    pw.write(content)
+    pw.close()
+  }
+
+}
